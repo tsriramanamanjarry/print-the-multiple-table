ab499b6192015023e38e54ef8a6542fe84f1974c
tsriramanamanjarry <143001940+tsriramanamanjarry@users.noreply.github.com>
2024-10-04 14:05:24 +0530

    Create tables.java

---
+++ tables.java
@@ -0,0 +1,10 @@
+class Tables {
+    public static void main(String[] args) {
+        int c =2;
+        
+        for(int i=1;i<=10;i++){
+           int  a = i*c;
+            System.out.println(i+"*"+c+"="+a);
+        }
+    }
+}
